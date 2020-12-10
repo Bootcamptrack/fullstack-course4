26294b6173086f32cb088e8939c72d0187c68ae1
FolusoSAMUEL <folusosamuel@gmail.com>
2020-12-10 06:12:27 +0100

    Module4 Assignment completed

--- assignments/assignment4/assignment4-solution-starter/harder/SpeakHello.js
+++ assignments/assignment4/assignment4-solution-starter/harder/SpeakHello.js
@@ -1,9 +1,13 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-
+var helloSpeaker = {};
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
+var speak =function () {
+  console.log("Hello" + name);
+}
+
 // See Lecture 52, part 1
 // var helloSpeaker =
 
